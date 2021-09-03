2132d4b75803796f981f57f704dae007be583a0f
abdhulla-k <abdhullak149@gmail.com>
2021-09-03 16:27:27 +0530

    learned some basic number functions

---
+++ Python/number.py
@@ -0,0 +1,32 @@
+# how to print both number and string
+
+print('Abdhu is a ' + str(20) + ' years old man')
+
+# how to print absolute value of a number
+
+print('absolute value of -2 is: ' + str(abs(-2)))
+
+# how to print the higest number from a list
+
+num = [1,3,2,4,8,6,7,0,8,9]
+print('Higest number from num is : '+ str(max(num)))
+
+# how to print lowest number from a list 
+
+print('lowest number of num is : ' + str(min(num)))
+
+# how to round a number
+
+print('rounded 2.2 to : '+ str(round(2.2)))
+
+# how to convert a number in to binary number
+# what the bin does is that it convert a perticular number to string
+
+k = 3
+print('binary number of '+str(k)+' is :  ' + str(bin(k)))
+
+
+
+
+"""This are some basic functions.But there are countless functions.
+import them to use"""
