14798d5d42d6d6bc1c748729e801c558222e0d24
abdhulla-k <abdhullak149@gmail.com>
2021-09-04 09:20:11 +0530

    Some currection in OutputAndInput.py

---
+++ Python/OutputAndInput.py
@@ -0,0 +1,19 @@
+# Out put method
+
+print('enything')
+
+# out put a variable string
+variable = 'string'
+print(variable)
+
+# out put string and int
+
+intiger = 1
+print(variable +'\n'+ str(intiger))
+
+
+######################################################################
+
+# how to input something fro user
+n = input("Enter a number")
+print("user input is: " + str(n))
