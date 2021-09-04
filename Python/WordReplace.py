727b465369ff732b7b628763ff1147195d8d1f8f
abdhulla-k <abdhullak149@gmail.com>
2021-09-04 10:00:11 +0530

    created a sample program that can replace a word as your like

---
+++ Python/WordReplace.py
@@ -0,0 +1,7 @@
+# A simple programe which replace a word to an another one as your like
+
+sentence = input('Enter your sentence: ')
+print('Your sentence is: ' + sentence)
+word1 = input('Enter the word to replace: ')
+word2 = input('Enter the word to replace it withe: ')
+print('new sentence is: ' + sentence.replace(word1, word2))
