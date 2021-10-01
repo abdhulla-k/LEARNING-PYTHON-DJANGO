faf8bd8efd90ece83448f723129a85c755e8be58
abdhulla-k <abdhullak149@gmail.com>
2021-10-01 22:35:17 +0530

    created user registration page

--- LearnDjango/sample/myapp/urls.py
+++ LearnDjango/sample/myapp/urls.py
@@ -8,5 +8,6 @@ from . import views              # imported views
 urlpatterns = [
     path('', views.index, name='index '),           # go to the views.py and create metod 'index'
                                                    # index is a function from views.py
-    path('counter', views.counter, name='counter')
+    path('counter', views.counter, name='counter'),
+    path('register', views.register, name='register')
 ]
--- LearnDjango/sample/myapp/views.py
+++ LearnDjango/sample/myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Features
 
 # Create your views here.
@@ -56,6 +58,32 @@ def index(request):
 
    return render(request, 'index.html',{'features':features})
 
+def register(request):
+   if request.method == 'POST':
+      username = request.POST['username']
+      email = request.POST['email']
+      password = request.POST['password']
+      password2 = request.POST['password2']
+
+      if password == password2:
+         # import redirect
+         # from django.contrib.auth.models import user, auth
+         # from django.contrib import messages
+         if User.objects.filter(email=email).exists():
+            messages.info(request, 'Email Already Used')
+            return redirect('register')
+         elif User.objects.filter(username = username):
+            messages.info(request, 'Username already used')
+            return redirect('register')
+         else:
+            user = User.objects.create_user(username = username, email = email, password = password)
+            user.save()
+            return redirect('login')
+      else:
+         messages.info(request, 'Password Not The Same')
+         return redirect('register')
+   else:
+      return render(request, 'register.html')
 
 def counter(request):
    text = request.POST['text']  # learned about GET and POST methods. POST is more safe
