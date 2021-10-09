dc4f4fbf435ab9b8253dfe4a0c2ed9b6da26b0ad
abdhulla-k <abdhullak149@gmail.com>
2021-10-09 16:53:47 +0530

    added an api in views.py to get the weather data. added the all data with html file.

--- weatherapp/weatherdetector/weather/views.py
+++ weatherapp/weatherdetector/weather/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
+        # add with api
+        # this api from 'openweathermap.org'
+        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=2160233e2dd654a19081df9efd8237a6').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code":str(json_data['sys']['country']),
+            "coordinate":str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            "temp":str(json_data['main']['temp']) + 'k',
+            "pressure":str(json_data['main']['pressure']),
+            "humidity":str(json_data['main']['humidity']),
+        }
     else:
         city = ''
-    return render(request, 'index.html', {'city': city})
+        data = {}
+    return render(request, 'index.html',{'city':city,'data':data})
