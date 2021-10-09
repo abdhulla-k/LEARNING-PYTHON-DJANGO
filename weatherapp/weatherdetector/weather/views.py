9a2ad810972b88504136e7deda63f622123c56c4
abdhulla-k <abdhullak149@gmail.com>
2021-10-09 16:01:02 +0530

    completed html file. modified method index.

--- weatherapp/weatherdetector/weather/views.py
+++ weatherapp/weatherdetector/weather/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        city = request.POST['city']
+    else:
+        city = ''
+    return render(request, 'index.html', {'city': city})
