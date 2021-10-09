67cf317b75f3c06a94108112c77b17c6a008b1e9
abdhulla-k <abdhullak149@gmail.com>
2021-10-09 15:15:21 +0530

    Created home url and added that to mainprojuct. some currection in index.html.

--- weatherapp/weatherdetector/weatherdetector/settings.py
+++ weatherapp/weatherdetector/weatherdetector/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'weatherdetector.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'template'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
