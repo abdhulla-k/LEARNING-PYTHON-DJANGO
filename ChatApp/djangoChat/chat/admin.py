e0061807da05eccb8783da0cdcab891d65dc55c5
abdhulla-k <abdhullak149@gmail.com>
2021-10-12 19:25:50 +0530

    registered the models (data bases) which created early

--- ChatApp/djangoChat/chat/admin.py
+++ ChatApp/djangoChat/chat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.db import models
+from .models import Room, Message
 
 # Register your models here.
+
+admin.site.register(Room)
+admin.site.register(Message)
